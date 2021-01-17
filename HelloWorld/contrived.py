7748718214a186d2d595051a9d5e6cdb56be476b
jboliv01 <57547273+jboliv01@users.noreply.github.com>
2021-01-17 15:27:02 -0500

    update

--- HelloWorld/contrived.py
+++ HelloWorld/contrived.py
@@ -1,9 +1,9 @@
 numbers = [1, 45, 31, 16, 60]
 
-#using else in a for loop
+# using else in a for loop
 
 for number in numbers:
-    if number % 8 == 0: #if number is divisible by 8
+    if number % 8 == 0:  # if number is divisible by 8
         # reject the list
         print("The numbers are unacceptable")
         break
